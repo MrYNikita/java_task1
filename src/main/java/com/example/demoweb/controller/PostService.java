1ef77fe439d8ed6e7b6c278a7193734d53d9f65b
Mr_Y_Nikita <Mr_Y_Nikita@gmail.com>
2021-09-15 20:53:55 +0500

    Update_2

--- src/main/java/com/example/demoweb/controller/PostService.java
+++ src/main/java/com/example/demoweb/controller/PostService.java
@@ -1,17 +1,25 @@
 package com.example.demoweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Date;
 
 @Service public class PostService {
+    List<Post> posts = new ArrayList<>();
+
     public List<Post> listAllPost() {
-        return Arrays.asList(
+        return posts;
+        /*return Arrays.asList(
                 new Post("Vini",new Date(),1),
                 new Post("Vidi",new Date(),142),
                 new Post("Vici",new Date(),987)
-        );
+        );*/
+    }
+    public void create(String text) {
+        posts.add(new Post(text,new Date(),0));
     }
 }
